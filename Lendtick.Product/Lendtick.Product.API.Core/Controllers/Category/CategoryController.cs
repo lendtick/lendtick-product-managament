2085604aebd99f4a2e10fff2d0742b1633fb5383
Wulung Triyanto <wulung_try@live.com>
2018-09-15 19:05:58 +0700

    Wulung-20180915-Product-Add Category Controllers

--- Lendtick.Product/Lendtick.Product.API.Core/Controllers/Category/CategoryController.cs
+++ Lendtick.Product/Lendtick.Product.API.Core/Controllers/Category/CategoryController.cs
@@ -37,5 +37,55 @@ namespace Lendtick.Product.API.Core.Controllers.Category
 
             return Ok(response);
         }
+
+        [HttpGet("secondcategory")]
+        [Produces("application/json")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(CategoryResponse))]
+        public async Task<ActionResult<CategoryResponse>> GetSecondCategory([FromQuery] string parentCategory)
+        {
+            IResultStatus result = new ResultStatus();
+            CategoryResponse response = new CategoryResponse();
+
+            ICategoryManager manager = new CategoryManager(parentCategory);
+            result = await manager.GetSecondCategory();
+
+            response.Status = result.IsSuccess;
+            response.Message = result.Message;
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(response);
+            }
+
+            response.Data = manager.Categories as List<string>;
+
+            return Ok(response);
+        }
+
+        [HttpGet("thirdcategory")]
+        [Produces("application/json")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(CategoryResponse))]
+        public async Task<ActionResult<CategoryResponse>> GetThirdCategory([FromQuery] string parentCategory)
+        {
+            IResultStatus result = new ResultStatus();
+            CategoryResponse response = new CategoryResponse();
+
+            ICategoryManager manager = new CategoryManager(parentCategory);
+            result = await manager.GetThirdCategory();
+
+            response.Status = result.IsSuccess;
+            response.Message = result.Message;
+
+            if (!result.IsSuccess)
+            {
+                return NotFound(response);
+            }
+
+            response.Data = manager.Categories as List<string>;
+
+            return Ok(response);
+        }
     }
 }
